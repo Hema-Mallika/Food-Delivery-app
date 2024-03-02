6982375cd7f01043357c4a4236701a41005ec3d0
Hema-Mallika <89988291+Hema-Mallika@users.noreply.github.com>
2024-03-03 00:18:38 +0530

    Update settings.py

--- Online_Food_Delivery/settings.py
+++ Online_Food_Delivery/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "NAME": os.environ.get("postgres"),
         "USER": os.environ.get("postgres"),
         "PASSWORD": os.environ.get("1234"),
-        "HOST": "localhost.app",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
